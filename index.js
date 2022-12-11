f796943c963288e7d9acf763bda0673ce5daaa76
Mark Roberts <marker2911@gmail.com>
2022-12-11 22:29:32 +0000

    Started adding js calculations and elements

---
+++ index.js
@@ -0,0 +1,60 @@
+console.log("hello")
+
+// HTML ELEMENTS
+
+//// INPUT FIELDS
+const billInput = document.getElementById("bill-num")
+const peopleInput = document.getElementById("people-num")
+
+
+//// BUTTONS
+const tipBtn5 = document.getElementById("5-tip-btn")
+const tipBtn10 = document.getElementById("10-tip-btn")
+const tipBtn15 = document.getElementById("15-tip-btn")
+const tipBtn25 = document.getElementById("25-tip-btn")
+const tipBtn50 = document.getElementById("50-tip-btn")
+const tipBtnCustom = document.getElementById("custom-tip-btn")
+
+const allTipBtns = document.querySelectorAll(".tip-btns")
+
+const resetBtn = document.getElementById("reset-btn")
+
+//// RESULT FIELDS
+
+const tipAmountResult = document.getElementById("tip-amount-num")
+const totalAmountResult = document.getElementById("total-amount-num")
+
+function clearResults() {
+    tipAmountResult.innerHTML = ""
+    totalAmountResult.innerHTML = ""
+}
+
+function calculateTip() {
+    clearResults()
+
+    billSubTotal = Number(billInput.value)
+    numPeople = Number(peopleInput.value)
+    activeTip = document.getElementsByClassName("active")[0].value;
+
+    console.log((billSubTotal * activeTip) / numPeople)
+
+    tipAmountResult.innerHTML = ((billSubTotal * activeTip) / numPeople)
+    totalAmountResult.innerHTML = ((billSubTotal + (billSubTotal * activeTip)) / numPeople)
+
+    // bill + (bill*tip) = total
+}
+
+function removeClass(elements, className) {
+    elements.forEach(element => {
+        element.classList.remove(className)
+    })
+}
+
+allTipBtns.forEach(tipBtn =>  {
+    tipBtn.addEventListener("click", function() {
+        removeClass(allTipBtns, "active")
+        tipBtn.classList.add("active")
+        calculateTip()
+    })
+})
+
